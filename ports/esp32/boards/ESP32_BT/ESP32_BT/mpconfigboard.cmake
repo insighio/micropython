086a8d4813b38933e5e89921ae183f959775b1dc
Nikolaos Ftylitakis <ftylitak@gmail.com>
2023-08-04 11:40:18 +0300

    updated board versions to be able to compile for ESP32 and ESP32_SPIRAM

--- ports/esp32/boards/ESP32_BT/ESP32_BT/mpconfigboard.cmake
+++
@@ -1,8 +0,0 @@
-set(SDKCONFIG_DEFAULTS
-    boards/sdkconfig.base
-    boards/sdkconfig.spiram
-    boards/sdkconfig.240mhz
-    boards/ESP32_BT/sdkconfig.board
-)
-
-#set(MICROPY_FROZEN_MANIFEST ${MICROPY_PORT_DIR}/boards/ESP32_BT/manifest.py)
--- ports/esp32/boards/ESP32_BT_SPIRAM/mpconfigboard.cmake
+++ ports/esp32/boards/ESP32_BT_SPIRAM/mpconfigboard.cmake
@@ -1,7 +1,8 @@
 set(SDKCONFIG_DEFAULTS
     boards/sdkconfig.base
+    boards/sdkconfig.spiram
     boards/sdkconfig.240mhz
-    boards/ESP32_BT/sdkconfig.board
+    boards/ESP32_BT_SPIRAM/sdkconfig.board
 )
 
 # set(MICROPY_FROZEN_MANIFEST ${MICROPY_PORT_DIR}/boards/ESP32_BT/manifest.py)
