76c002d6f550bf24532dee8f21b73f848be21ded
Nikolaos Ftylitakis <ftylitak@gmail.com>
2025-04-02 13:50:53 +0300

    use different settings for 4MB and 8MB esp32s3

---
+++ ports/esp32/boards/GENERIC_S3_SPIRAM_4MB/mpconfigboard.cmake
@@ -0,0 +1,9 @@
+set(IDF_TARGET esp32s3)
+
+set(SDKCONFIG_DEFAULTS
+    boards/sdkconfig.base
+    boards/sdkconfig.spiram_sx
+    boards/sdkconfig.240mhz
+    boards/GENERIC_S3_SPIRAM/sdkconfig.board
+    boards/sdkconfig.usb
+)
